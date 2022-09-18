3393464e8fb33e0b4c2ef502bb83470eccc607fd
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-09-18 13:55:04 +0530

    added setup files

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -3,6 +3,11 @@ import Loading from "./Loading";
 import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 function App() {
+  const response = fetch(url).then((response) => {response.json()
+
+  })
+  catch(error){ });   
+
   return (
     <>
       <h2>Tours Project Setup</h2>
