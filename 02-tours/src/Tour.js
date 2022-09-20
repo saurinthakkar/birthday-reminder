488d5be46543ce2650148f167e72b359c5b45166
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-09-20 20:58:46 +0530

    feat:added modular code

--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ tour, removeTour }) => {
+  console.log("Selected tour", tour);
+  const [isReadMore, setIsReadMore] = useState(true);
+
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  const { id, name, info, image, price } = tour;
+
+  return (
+    <article className="single-tour" key={id}>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {isReadMore ? info.slice(0, 150) : info}
+          <button onClick={toggleReadMore}>
+            {isReadMore ? "..read more" : "show less"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
--- 02-tours/src/Tours.js
+++ 02-tours/src/Tours.js
@@ -1,38 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Tour from "./Tour";
 
 const Tours = ({ tours, removeTour }) => {
-  console.log("toursData", tours);
-  //const []
-  const [isReadMore, setIsReadMore] = useState(true);
-
-  const toggleReadMore = () => {
-    setIsReadMore(!isReadMore);
-  };
   return (
     <>
       {tours.map((tour) => {
-        const { id, name, info, image, price } = tour;
-        return (
-          <article className="single-tour" key={id}>
-            <img src={image} alt={name} />
-            <footer>
-              <div className="tour-info">
-                <h4>{name}</h4>
-                <h4 className="tour-price">${price}</h4>
-              </div>
-              <p>
-                {isReadMore ? info.slice(0, 150) : info}
-                <button onClick={toggleReadMore}>
-                  {isReadMore ? "..read more" : "show less"}
-                </button>
-              </p>
-              <button className="delete-btn" onClick={() => removeTour(id)}>
-                Not Interested
-              </button>
-            </footer>
-          </article>
-        );
+        return <Tour tour={tour} removeTour={removeTour} />;
       })}
     </>
   );
