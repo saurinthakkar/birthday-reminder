c08ec44ee1df89a870632180a7acf40eb13a62e2
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-09-28 17:07:43 +0530

    feat:filter added with menu

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -4,7 +4,68 @@ import Categories from "./Categories";
 import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [foodItems, setFoodItems] = useState(items);
+  console.log("food", foodItems);
+  const checkcategory = (e) => {
+    let selectedCategory = e.target.innerHTML;
+
+    console.log("select category", selectedCategory);
+
+    if (selectedCategory === "all") {
+      setFoodItems(items);
+    } else {
+      const selectedFoodItems = items.filter(
+        (foodItem) => foodItem.category === selectedCategory
+      );
+      setFoodItems(selectedFoodItems);
+      console.log("selectedfooditem", selectedFoodItems);
+    }
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Hello menu</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="filter-btn"
+            onClick={(e) => checkcategory(e)}
+          >
+            all
+          </button>
+          <button
+            type="button"
+            className="filter-btn"
+            onClick={(e) => checkcategory(e)}
+          >
+            breakfast
+          </button>
+          <button
+            type="button"
+            className="filter-btn"
+            onClick={(e) => checkcategory(e)}
+          >
+            lunch
+          </button>
+          <button
+            type="button"
+            className="filter-btn"
+            onClick={(e) => checkcategory(e)}
+          >
+            shakes
+          </button>
+        </div>
+        <div className="section-center">
+          {foodItems.map((item) => (
+            <Menu item={item} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/src/Menu.js
+++ 05-menu/src/Menu.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ item }) => {
+  //console.log("items", item);
+  return (
+    <article className="menu-item">
+      <img src={item.img} className="photo" alt={item.title} />
+      <div className="item-info">
+        <header>
+          <h4>{item.title}</h4>
+          <h4 className="price">{item.price}</h4>
+        </header>
+        <p className="item-text">{item.desc}</p>
+      </div>
+    </article>
+  );
 };
 
 export default Menu;
